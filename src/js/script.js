90b910e7558659cf9943812da4a74ff6bbecd6ff
enzo <enzolibman@gmail.com>
2026-03-27 09:50:33 -0300

    feat: declaraçoens de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// declaraçoens e varieaveis
+//var let e const
+
+var nome="fiap";
+console.log(nome);
+var nome="dev";
+console.log(nome);
+
+if(true){
+    var avatar="fiapinho";
+}
+
+console.log(avatar);
+
